1af7850ada76189dc5ab8bd36cc95767c5e72447
zakariae77 <37230416+zakariae77@users.noreply.github.com>
2021-10-13 08:42:02 -0700

    Add files via upload

---
+++ login/prix.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>login</title>
+    <link rel="stylesheet" href="css/prix.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="container">
+        <h1>9<sub>.99$ per month</sub></h1>
+        <input class="btn_sub" type="button" value="try 14 days for only 0.99$">
+        <ul>
+            <li>Créez des CV en illimité</li>
+            <li>Créez des CV en illimité</li>
+            <li>Créez des CV en illimité</li>
+            <li>Créez des CV en illimité</li>
+            <li>Créez des CV en illimité</li>
+        </ul>
+        
+    </div>
+</body>
+</html>
---
+++ login/registration.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Create acount</title>
+    <link rel="stylesheet" href="css/registeration.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="login">
+        <h1>Create acount</h1>
+        <form  action="">
+            <label for="fname">First name </label>
+            <input type="text" name="fname" id="">
+            <label for="lname">Last name</label>
+            <input type="text" name="lname" id="">
+            <label for="email">Email*</label>
+            <input type="email" name="email" id="">
+            <label for="password">Password*</label>
+            <input type="password" name="password" id="">
+            
+            <input class="btn_sub" type="submit" value="create">
+            <p>*Do you already have an acount ?</p>
+            <p><a href="login.php"> *Login Know </a></p>
+        </form>
+    </div>
+</body>
+</html>
